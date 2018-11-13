9086cb082c968dabbbf2ca55cf4726d4b2f8df89
Lucy Suddenly <suddenlylucy@gmail.com>
2018-11-13 00:09:10 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -32,7 +32,7 @@ def turn(board)
   input = gets.strip
   index = input_to_index(input)
   if valid_move?(board, index)
-    move(board, index, character)
+    move(board, index, character = "X")
   else 
     turn(board)
   end
